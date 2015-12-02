67d1c389bd5a7949303e8f45ca99d30206dd7936
Mal Morrow <malmorrow@gmail.com>
2015-12-02 22:03:44 +0200

    Changed ward url pattern

--- scout/urls.py
+++ scout/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     url(r'^parent/$', views.CreateParent.as_view(), name='parent-create'),
     url(r'^parent/(?P<pk>\d+)/$', views.ParentDetail.as_view(), name='parent-detail'),
-    url(r'^ward_create/$', views.CreateWard.as_view(), name='ward-create'),
+    url(r'^ward/$', views.CreateWard.as_view(), name='ward-create'),
     url(r'^ward/(?P<pk>\d+)/$', views.WardDetail.as_view(), name='ward-detail'),
 ]
